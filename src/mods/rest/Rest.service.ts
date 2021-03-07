d3a7505bcb519ca469a1a9415a5e44e5c5c491f1
feddot2517@gmail.com <8D2g1101>
2021-03-07 14:26:54 +0600

    rest service search by regex

--- src/mods/rest/Rest.service.ts
+++ src/mods/rest/Rest.service.ts
@@ -4,11 +4,14 @@ import { addComponent } from '../../treenity/context/context-db';
 import { meta } from '../../treenity/meta/meta.model';
 import {useApp} from "../../treenity/react/useApp";
 import mongoService from 'feathers-mongodb';
+import search from 'feathers-mongodb-fuzzy-search'
 import createClientDb from "../mongo/mongod";
 
 const RestServiceMeta = meta('rest.service', types.model({
   baseUrl: types.string,
   collectionName: types.string,
+  whitelist: types.array(types.string),
+  allowedFields: types.array(types.string)
 }));
 
 addComponent(RestServiceMeta, 'service', {}, ({ value }) => {
@@ -20,7 +23,18 @@ addComponent(RestServiceMeta, 'service', {}, ({ value }) => {
     db.then(db => {
       app.use(value.baseUrl, mongoService({
         Model: db.collection(value.collectionName),
+        whitelist: value.whitelist
       }));
+
+      app.service(value.baseUrl).hooks({
+        before: {
+          find: [
+            search({
+              fields: value.allowedFields
+            })
+          ]
+        }
+      })
     });
 
     return () => undefined;
