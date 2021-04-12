66b9e353009a08c62fc3c0f98881763e4408301e
Kurilov <kri.zka@gmail.com>
2021-04-13 00:22:00 +0500

    use parcel2

--- src/mods/solana/config.ts
+++ src/mods/solana/config.ts
@@ -1,14 +1,7 @@
-export const MONGO_SERVICE_URL = process.env.NODE_ENV === 'production'
-  ? '/solana/api'
-  : 'http://localhost:3100/solana';
+export const MONGO_SERVICE_URL = '/solarea/api';
 
-export const SESSION_SERVICE_URL = process.env.NODE_ENV === 'production'
-    ? '/solarea/sessionid'
-    : 'http://localhost:3100/solarea/sessionid';
+export const SESSION_SERVICE_URL = '/solarea/sessionid';
 
-
-export const VALIDATE_SERVICE_URL = process.env.NODE_ENV === 'production'
-    ? '/solarea/validate'
-    : 'http://localhost:3100/solarea/validate';
+export const VALIDATE_SERVICE_URL = '/solarea/validate';
 
 export const BITQUERY_BASE = 'https://graphql.bitquery.io/';
