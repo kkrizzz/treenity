61df3ed7efecbe03cf6e2ec3e1590a3151bf208a
Kurilov <kri.zka@gmail.com>
2021-04-13 10:31:40 +0500

    deploy scripts update

--- scripts/deploy-sol.sh
+++ scripts/deploy-sol.sh
@@ -2,9 +2,13 @@
 
 set -e
 
+rm -rf dist/solana-prod dist/server-prod
 yarn sol:build
 yarn server:build
 
-rsync -azvh --delete dist/solana-prod dist/server-prod.js config package.json $F1:/var/www/sol.treenity.pro/
+cp -R public/* dist/solana-prod/
 
-ssh $F1 ". .nvm/nvm.sh  && pm2 restart treenity-exp"
+rsync -azvh --delete dist/solana-prod  $F1:/var/www/sol.treenity.pro/
+rsync -azvh dist/server-prod/ config package.json $F1:/solarea/
+
+ssh $F1 ". .nvm/nvm.sh  && pm2 restart solarea"
