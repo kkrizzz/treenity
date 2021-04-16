70c627bda2556bce34e0708e5caff7eb6b58315a
Kurilov <kri.zka@gmail.com>
2021-04-16 21:56:37 +0500

    add demo key for bit-query

--- src/mods/solana/hooks/useBitQuery.tsx
+++ src/mods/solana/hooks/useBitQuery.tsx
@@ -1,27 +1,27 @@
 import * as React from 'react';
-import {BITQUERY_BASE} from "../config";
+import { BITQUERY_BASE } from '../config';
 
 export function useBitQuery(code: string) {
-    const [res, setRes] = React.useState<{data: any}>({data: ''});
-    const [isLoading, setIsLoading] = React.useState<boolean>(true);
+  const [res, setRes] = React.useState<{ data: any }>({ data: '' });
+  const [isLoading, setIsLoading] = React.useState<boolean>(true);
 
-    React.useEffect(() => {
-        setIsLoading(true);
+  React.useEffect(() => {
+    setIsLoading(true);
 
-        (async function() {
-            const req = await fetch(BITQUERY_BASE, {
-                method: 'POST',
-                cache: 'no-cache',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify({query: code, variables: {}}),
-            });
-            setRes(await req.json());
-            setIsLoading(false);
-        })()
+    (async function () {
+      const req = await fetch(BITQUERY_BASE, {
+        method: 'POST',
+        cache: 'no-cache',
+        headers: {
+          'X-API-KEY': 'BQYuBhWWLDMGNQ7JbDU9BjOoYycCixU9',
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ query: code, variables: {} }),
+      });
+      setRes(await req.json());
+      setIsLoading(false);
+    })();
+  }, [code]);
 
-    }, [code]);
-
-    return [res, isLoading]
+  return [res, isLoading];
 }
