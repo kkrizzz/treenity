7d5fcbe34b94c2deb08d315574383ba2d5ee41fa
feddot2517@gmail.com <8D2g1101>
2021-04-19 21:05:50 +0600

    email subscribe

---
+++ src/server/solana/email-subscribe.ts
@@ -0,0 +1,25 @@
+import { Application } from '@feathersjs/express'
+
+export function emailSubscribeRoute(app: Application) {
+    app.post('/solarea/subscribe', async (req, res) => {
+        try {
+            const { email } = req.body;
+            const ip = req.headers['x-real-ip'];
+
+            if(!ip) throw new Error('no ip');
+            if(!email) throw new Error('no email');
+            if( !(/\S+@\S+\.\S+/).test(email) ) throw new Error('validation error');
+
+            const service = app.service('mails');
+
+            if((await service.find({query: {ip}}))[0]) throw new Error('this ip already subscribed')
+            if((await service.find({query: {email}}))[0]) throw new Error('this email already subscribed')
+
+            await service.create({email, ip, timestamp: new Date()})
+            res.status(200).send('OK');
+        }
+        catch (e) {
+            res.status(401).send(e.toString())
+        }
+    })
+}
--- src/server/solana/routes-startup.ts
+++ src/server/solana/routes-startup.ts
@@ -1,8 +1,10 @@
 import { Application } from '@feathersjs/express';
 import { sessionIdRoute } from './session-id';
 import { prettierFormat } from '../../mods/solarea/server/prettier';
+import {emailSubscribeRoute} from "./email-subscribe";
 
 export function routesStartup(app: Application) {
   sessionIdRoute(app);
   prettierFormat(app);
+  emailSubscribeRoute(app)
 }
