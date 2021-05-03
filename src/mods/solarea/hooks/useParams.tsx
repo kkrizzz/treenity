634441dbc51637850167159559a595e814f92828
Kurilov <kri.zka@gmail.com>
2021-05-03 14:37:00 +0500

    use domain name as ID if no ID set

--- src/mods/solarea/hooks/useParams.tsx
+++ src/mods/solarea/hooks/useParams.tsx
@@ -1,33 +1,39 @@
 import useLocation from './useLocation';
 
-// match url of format: [subdomain].domain.com/[id]/[name]/[context]
-// const part = '/([\\w\\d\\-:\\.]+)'; // '/
-// const urlMatcher = new RegExp(
-//   `^http?:\/${part}(:\\d+)?${part}((${part}(${part})?)?(\\?.*)?)?$`,
-//   'i'
-// );
-// const TRANSACTION = 'transaction';
-
 const DEFAULT = 'default';
 const REACT = 'react';
 
 export default function useParams() {
   const location = useLocation();
 
-  const names = location.hostname.split('.');
+  const names = location.hostname.split('.').reverse();
   const paths = location.pathname.split('/').filter((i) => !!i);
-  const hasSubdomain = names.length === 3; // 2 - localhost 3 - .domen
 
-  let addr, name, context;
+  let addr,
+    name,
+    context;
 
-  if (hasSubdomain) {
+  // has 2 or more subdomains
+  if (names.length >= 4) {
+    let first,
+      second;
+    [first, second, addr, context] = names;
+    addr = `${second}.${first}`;
+    context = paths[0];
+    // has one subdomain
+  } else if (names.length === 3) {
     addr = names[0];
     name = paths[0];
     context = paths[1];
+    // just domain name - 'meta.store'
+  } else if (paths.length === 0) {
+    addr = `${names[1]}.${names[0]}`;
+    // url site.name/addr/name/context - address with name in context
   } else {
     addr = paths[0];
     name = paths[1];
     context = paths[2];
   }
-  return [addr || 'root', name || DEFAULT, context || REACT]
+  // some defaults if needed
+  return [addr, name || DEFAULT, context || REACT];
 }
