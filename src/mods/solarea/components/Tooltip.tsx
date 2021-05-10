b9bd6f3b3ed0aa1b12766d2ce1a7b93a32b76e58
vladkuraev84 <vladkuraev84@gmail.com>
2021-05-10 18:02:13 +0300

    changed structure for menu-markup

---
+++ src/mods/solarea/components/Tooltip.tsx
@@ -0,0 +1,20 @@
+import React, { ReactNode } from 'react';
+
+import './Tooltip.scss';
+
+export default function Tooltip(props: {
+  wrapperClassName: string;
+  children: ReactNode;
+  title: string;
+  position: 'left' | 'right' | 'top' | 'bottom';
+  onClick: () => void;
+}) {
+  return (
+    <div className={props.wrapperClassName} onClick={props.onClick}>
+      {props.children}
+      <div className={`sol-tooltip ${props.position}`}>
+        <span>{props.title}</span>
+      </div>
+    </div>
+  );
+}
--- src/mods/solarea/markup/menuMarkup/MenuMarkup.tsx
+++ src/mods/solarea/markup/menuMarkup/MenuMarkup.tsx
@@ -1,52 +1,104 @@
 import React from 'react';
 
-import './menuMarkup.scss'
+import './menuMarkup.scss';
 import { Icon } from '../../components/Icon';
+import Tooltip from '../../components/Tooltip';
 
-type Props = {}
+type Props = {};
+
+const menuItems = [
+  {
+    icon: 'burger2',
+    title: 'Menu',
+    onClick: () => {
+      console.log('qwe');
+    },
+  },
+  {
+    icon: 'reload',
+    title: 'Reload',
+    onClick: () => {
+      console.log('qwe');
+    },
+  },
+  {
+    icon: 'save',
+    title: 'Save',
+    onClick: () => {
+      console.log('qwe');
+    },
+  },
+  {
+    icon: 'play',
+    title: 'Start',
+    onClick: () => {
+      console.log('qwe');
+    },
+  },
+  {
+    icon: 'rewind',
+    title: 'Rewind',
+    onClick: () => {
+      console.log('qwe');
+    },
+  },
+];
+
+const menuItemsSub = [
+  {
+    icon: 'info',
+    title: 'Information',
+    onClick: () => {
+      console.log('qwe');
+    },
+  },
+  {
+    icon: 'settings',
+    title: 'Settings',
+    onClick: () => {
+      console.log('qwe');
+    },
+  },
+];
 
 export function MenuMarkup({}: Props) {
-    return (
-          <div className="menu-markup">
-              <ul className="menu-markup-list">
-                  <li>
-                      <button className="btn btn-menu-markup" title="Menu">
-                          <Icon name="burger2" />
-                      </button>
-                  </li>
-                  <li>
-                      <button className="btn btn-menu-markup" title="Reload">
-                          <Icon name="reload" />
-                      </button>
-                  </li>
-                  <li>
-                      <button className="btn btn-menu-markup" title="Save">
-                          <Icon name="save" />
-                      </button>
-                  </li>
-                  <li>
-                      <button className="btn btn-menu-markup" title="Start">
-                          <Icon name="play" />
-                      </button>
-                  </li>
-                  <li>
-                      <button className="btn btn-menu-markup" title="Start">
-                          <Icon name="rewind" />
-                      </button>
-                  </li>
-              </ul>
-              <ul className="menu-markup-list-sub">
-                  <li>
-                      <button className="btn btn-menu-markup" title="Information">
-                          <Icon name="info" />
-                      </button>
-                  </li>
-                  <li>
-                      <button className="btn btn-menu-markup" title="Settings">
-                          <Icon name="settings" />
-                      </button>
-                  </li>
-              </ul>
-          </div>
-    );
+  return (
+    <div className="sol-menu-markup">
+      <ul className="sol-menu-markup-list">
+        {menuItems.map((item, key) => {
+          return (
+            <li>
+              <Tooltip
+                wrapperClassName="sol-btn-menu-markup sol-tooltip-wr"
+                title={item.title}
+                position="right"
+                key={key}
+                onClick={item.onClick}
+              >
+                <Icon name={item.icon} />
+              </Tooltip>
+            </li>
+          );
+        })}
+      </ul>
+
+      <ul className="sol-menu-markup-list-sub">
+        {menuItemsSub.map((i, idx) => {
+          return (
+            <li>
+              <Tooltip
+                wrapperClassName="sol-btn-menu-markup sol-tooltip-wr"
+                title={i.title}
+                position="right"
+                key={idx}
+                onClick={i.onClick}
+              >
+                <Icon name={i.icon} />
+              </Tooltip>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
 }
