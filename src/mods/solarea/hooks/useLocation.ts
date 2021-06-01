cde1fe7a5d0848842890c13733e267969bc722a4
fed <feddot2517@gmail.com>
2021-06-01 15:06:39 +0600

    useLocation hook fix. removeAllUseCSS only when arguments passed

--- src/mods/solarea/hooks/useLocation.ts
+++ src/mods/solarea/hooks/useLocation.ts
@@ -1,27 +1,27 @@
-import React from "react";
+import React from 'react';
 
 history.pushState = function pushState() {
-    History.prototype.pushState.apply(history, arguments);
-    globalThis.removeAlluseCSSprop();
-    window.dispatchEvent(new Event('pushState'));
+  History.prototype.pushState.apply(history, arguments);
+  if (arguments[4]) globalThis.removeAlluseCSSprop();
+  window.dispatchEvent(new Event('pushState'));
 };
 
 export default function useLocation() {
-    const [location, setLocation] = React.useState(window.location);
+  const [location, setLocation] = React.useState(window.location);
 
-    const handleHashChange = React.useCallback(() => {
-        // just window.location not worked
-        setLocation(Object.assign({}, window.location));
-    }, []);
+  const handleHashChange = React.useCallback(() => {
+    // just window.location not worked
+    setLocation(Object.assign({}, window.location));
+  }, []);
 
-    React.useEffect(() => {
-        window.addEventListener("pushState", handleHashChange);
-        window.addEventListener("popstate", handleHashChange);
-        return () => {
-            window.removeEventListener("pushState", handleHashChange)
-            window.removeEventListener("popstate", handleHashChange)
-        }
-    }, [handleHashChange]);
+  React.useEffect(() => {
+    window.addEventListener('pushState', handleHashChange);
+    window.addEventListener('popstate', handleHashChange);
+    return () => {
+      window.removeEventListener('pushState', handleHashChange);
+      window.removeEventListener('popstate', handleHashChange);
+    };
+  }, [handleHashChange]);
 
-    return location
+  return location;
 }
