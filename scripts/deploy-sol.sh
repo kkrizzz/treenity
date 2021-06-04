7521a5b0d80cd4bb4509f11941a4e2f591d069ef
Kurilov <kri.zka@gmail.com>
2021-06-04 15:57:52 +0300

    fix deploy script

--- scripts/deploy-sol.sh
+++ scripts/deploy-sol.sh
@@ -8,7 +8,12 @@ yarn server:build
 cp -R public/* dist/solarea-prod/
 
 SSH_STR=$F1
-SSH_OPTS="-o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -i $SSH_PRIVATE"
+SSH_OPTS="-o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null"
+
+if [ -n "$SSH_PRIVATE" ]; then
+  SSH_OPTS+=" -i $SSH_PRIVATE"
+fi
+
 SSH="ssh $SSH_OPTS"
 SCP="scp $SSH_OPTS"
 
@@ -19,7 +24,7 @@ run_rsync() {
   --progress "$@"
 }
 
-run_rsync --delete dist/solarea-prod/ $F1:/var/www/sol.treenity.pro/
-run_rsync dist/server-prod/ config package.json $F1:~/solarea/
+run_rsync --delete dist/solarea-prod/ $SSH_STR:/var/www/sol.treenity.pro/
+run_rsync dist/server-prod/ config package.json $SSH_STR:~/solarea/
 
-$SSH $F1 ". .nvm/nvm.sh  && pm2 restart solarea"
+$SSH $SSH_STR ". .nvm/nvm.sh  && pm2 restart solarea"
