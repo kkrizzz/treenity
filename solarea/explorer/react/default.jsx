7a7099d07f825b96ad6c4e81d377e2e0d89dd0ce
feddot2517 <8d2G1101>
2021-07-30 17:35:56 +0600

    Main page (cluster and transactions stats)

--- solarea/explorer/react/default.jsx
+++ solarea/explorer/react/default.jsx
@@ -1,4 +1,5 @@
 const BulmaCard = render('dev', 'bulma-card');
+const TwoColumn = render('dev', 'two-column');
 
 const LPS = 0.000000001;
 
@@ -28,6 +29,18 @@ const ProgressBar = () => {
   );
 };
 
+const SmallCard = ({ head, children, foot }) => {
+  return (
+    <div>
+      <div className="bu-is-size-5 bu-has-text-black">{head}</div>
+      <div>
+        <div className="bu-is-size-3 bu-has-text-primary">{children}</div>
+      </div>
+      <div className="bu-is-size-5 bu-has-text-primary">{foot}</div>
+    </div>
+  );
+};
+
 const StakeStats = ({ voteAccounts, currentSupply }) => {
   const { current, delinquent } = voteAccounts;
   const { total: totalSupply } = currentSupply.value;
@@ -38,74 +51,82 @@ const StakeStats = ({ voteAccounts, currentSupply }) => {
   const delinquentStake = delinquent.reduce(sumStake, 0);
 
   return (
-    <div>
-      <div class="bu-is-size-5 bu-has-text-black">Active Stake</div>
-      <div>
-        <div class="bu-is-size-3 bu-has-text-primary">
-          {humanizeFormatter(activeStake * LPS, 1)}
-          <span className="bu-is-size-3 bu-has-text-black">/</span>
-          <span className="bu-is-size-3 bu-has-text-black">
-            {humanizeFormatter(totalSupply * LPS, 1)}
-          </span>
+    <SmallCard
+      head="Active Stake"
+      foot={
+        <div>
+          <span className="bu-is-size-5 bu-has-text-black">Delinquent stake: </span>
+          {(100 - (activeStake / (activeStake + delinquentStake)) * 100).toFixed(2)}%
         </div>
+      }
+    >
+      <div class="bu-is-size-3 bu-has-text-primary">
+        {humanizeFormatter(activeStake * LPS, 1)}
+        <span className="bu-is-size-3 bu-has-text-black">/</span>
+        <span className="bu-is-size-3 bu-has-text-black">
+          {humanizeFormatter(totalSupply * LPS, 1)}
+        </span>
       </div>
-      <div className="bu-is-size-5 bu-has-text-primary">
-        <span className="bu-is-size-5 bu-has-text-black">Delinquent stake: </span>
-        {(100 - (activeStake / (activeStake + delinquentStake)) * 100).toFixed(2)}%
-      </div>
-    </div>
+    </SmallCard>
   );
 };
 
 const PriceStats = ({ coinData }) => {
   const marketData = coinData.market_data;
   const priceChangePercentage24h = marketData.price_change_percentage_24h;
+  const priceChangePositive = priceChangePercentage24h >= 0;
+
+  const className = `bu-is-size-5 ${
+    priceChangePositive ? 'bu-has-text-success' : 'bu-has-text-danger'
+  }`;
+
   return (
-    <div>
-      <div className="bu-is-size-5 bu-has-text-black">Price</div>
-      <div>
-        <div className="bu-is-size-3 bu-has-text-primary">
-          ${marketData.current_price.usd}
-          <span className="bu-is-size-5 bu-has-text-black">
-            {priceChangePercentage24h >= 0 ? '↑' : '↓'}
-          </span>
-          <span className="bu-is-size-5 bu-has-text-black">
-            {priceChangePercentage24h.toFixed(2)}%
-          </span>
+    <SmallCard
+      head="Price"
+      foot={
+        <div>
+          <span className="bu-is-size-5 bu-has-text-black">24h Vol $</span>
+          {humanizeFormatter(marketData.total_volume.usd, 1)}
         </div>
+      }
+    >
+      <div className="bu-is-size-3 bu-has-text-primary">
+        ${marketData.current_price.usd}
+        <span className={className}>{priceChangePositive ? '↑' : '↓'}</span>
+        <span className={className}>{priceChangePercentage24h.toFixed(2)}%</span>
       </div>
-      <div className="bu-is-size-5 bu-has-text-primary">
-        <span className="bu-is-size-5 bu-has-text-black">24h Vol $</span>
-        {humanizeFormatter(marketData.total_volume.usd, 1)}
-      </div>
-    </div>
+    </SmallCard>
   );
 };
 
 const SupplyStats = ({ currentSupply }) => {
   const { circulating, total: totalSupply } = currentSupply.value;
+
   return (
-    <div>
-      <div class="bu-is-size-5 bu-has-text-black">Circulating Supply</div>
-      <div>
-        <div class="bu-is-size-3 bu-has-text-primary">
-          {humanizeFormatter(circulating * LPS, 1)}
-          <span className="bu-is-size-3 bu-has-text-black">/</span>
-          <span className="bu-is-size-3 bu-has-text-black">
-            {humanizeFormatter(totalSupply * LPS, 1)}
-          </span>
+    <SmallCard
+      head="Circulating Supply"
+      foot={
+        <div>
+          {((circulating / totalSupply) * 100).toFixed(1)}%{' '}
+          <span className="bu-is-size-5 bu-has-text-black">is circulating</span>
         </div>
-      </div>
-      <div class="bu-is-size-5 bu-has-text-primary">
-        {((circulating / totalSupply) * 100).toFixed(1)}%{' '}
-        <span className="bu-is-size-5 bu-has-text-black">is circulating</span>
-      </div>
-    </div>
+      }
+    >
+      {humanizeFormatter(circulating * LPS, 1)}
+      <span className="bu-is-size-3 bu-has-text-black">/</span>
+      <span className="bu-is-size-3 bu-has-text-black">
+        {humanizeFormatter(totalSupply * LPS, 1)}
+      </span>
+    </SmallCard>
   );
 };
 
 add(() => {
   const [currentSupply, isSupplyLoading] = solarea.useSolanaRpc('getSupply', 'finalized');
+  const [epochInfo, isEpochInfoLoading] = solarea.useSolanaRpc('getEpochInfo');
+  const [recentPerformance, isRecentPerformanceLoading] = solarea.useSolanaRpc(
+    'getRecentPerformanceSamples',
+  );
   const [voteAccounts, isVoteAccountsLoading] = solarea.useSolanaRpc(
     'getVoteAccounts',
     'finalized',
@@ -116,34 +137,72 @@ add(() => {
     () => fetch('https://api.coingecko.com/api/v3/coins/solana').then((res) => res.json()),
   );
 
-  const isLoading = isSupplyLoading || isVoteAccountsLoading;
+  const cards = [SupplyStats, StakeStats, PriceStats];
+
+  const isLoading = isSupplyLoading || isVoteAccountsLoading || isSolanaDataLoading;
   return (
     <Render id="explorer" name="layout">
       <div className="bu-container bu-is-max-desktop">
         <BulmaCard>
-          {isLoading ? (
-            <div className="bu-column">
-              <div className="bu-box bu-is-primary">
+          <div className="bu-columns">
+            {isLoading ? (
+              <div class="bu-column bu-box">
                 <ProgressBar />
               </div>
-            </div>
+            ) : (
+              cards.map((Card) => (
+                <div className="bu-column">
+                  <div class="bu-box">
+                    <Card
+                      coinData={coinData}
+                      currentSupply={currentSupply}
+                      voteAccounts={voteAccounts}
+                    />
+                  </div>
+                </div>
+              ))
+            )}
+          </div>
+        </BulmaCard>
+        <BulmaCard header="Cluster stats">
+          {isEpochInfoLoading ? (
+            <ProgressBar />
           ) : (
-            <div className="bu-columns">
-              <div className="bu-column">
-                <div className="bu-box bu-is-primary">
-                  <SupplyStats currentSupply={currentSupply} />
-                </div>
-              </div>
-              <div className="bu-column">
-                <div className="bu-box bu-is-primary">
-                  <StakeStats currentSupply={currentSupply} voteAccounts={voteAccounts} />
-                </div>
-              </div>
-              <div className="bu-column">
-                <div className="bu-box bu-is-primary">
-                  <PriceStats coinData={coinData} />
-                </div>
-              </div>
+            <div>
+              <TwoColumn
+                first="Slot"
+                link={`/block/${epochInfo.absoluteSlot}`}
+                second={epochInfo.absoluteSlot}
+              />
+              <TwoColumn first="Block height" second={epochInfo.blockHeight} />
+              <TwoColumn first="Epoch" second={epochInfo.epoch} />
+              <TwoColumn
+                first="Epoch progress"
+                second={((epochInfo.slotIndex / epochInfo.slotsInEpoch) * 100).toFixed(1) + '%'}
+              />
+            </div>
+          )}
+        </BulmaCard>
+        <BulmaCard header="Transaction stats">
+          {isEpochInfoLoading || isRecentPerformanceLoading ? (
+            <ProgressBar />
+          ) : (
+            <div>
+              <TwoColumn first="Total transactions" second={epochInfo.transactionCount} />
+              <TwoColumn
+                first="TPS"
+                second={Math.floor(
+                  recentPerformance[0].numTransactions / recentPerformance[0].samplePeriodSecs,
+                )}
+              />{' '}
+              <TwoColumn
+                first="Average TPS (30 min)"
+                second={Math.floor(
+                  recentPerformance
+                    .slice(0, 60)
+                    .reduce((acc, val) => acc + val.numTransactions, 0) / 3600,
+                )}
+              />
             </div>
           )}
         </BulmaCard>
