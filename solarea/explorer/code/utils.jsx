f0fd163eb85b5c528ac7266871df74ce42866a7a
feddot2517 <8d2G1101>
2021-08-04 16:03:32 +0600

    tokens page

--- solarea/explorer/code/utils.jsx
+++ solarea/explorer/code/utils.jsx
@@ -7,3 +7,42 @@ exports.lpsRound = (lamports, digits = 4) => {
 exports.numberWithSpaces = (x) => {
   return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
 };
+
+exports.tokenRegExp = new RegExp(`  <td class="stakes-td">
+      <!-- incremented number by order in the list -->
+      <span class="color-lighten">
+.*
+      <\\/span>
+  <\\/td>
+  <td class="stakes-td">
+
+<a class="text-truncate" data-test="token_link" href="\\/tokens\\/.*">(.*)<\\/a>
+  <\\/td>
+  <td class="stakes-td">
+
+<a data-test="address_hash_link" href="\\/address\\/(.*)">
+<span class="contract-address" data-address-hash=".*">
+
+
+      <span class="d-none d-md-none d-xl-inline">.*<\\/span>
+      <span class="d-md-inline-block d-xl-none">.*<\\/span>
+
+
+<\\/span>
+
+<\\/a>
+
+  <\\/td>
+
+  <td class="stakes-td">
+
+      <span data-test="token_supply">(.*)<\\/span>
+ .*
+  <\\/td>
+  <td class="stakes-td">
+    <span class="mr-4">
+      <span data-test="transaction_count">
+.*
+    <\\/span>
+  <\\/td>
+<\\/tr>`);
--- solarea/explorer/react/tokens.jsx
+++ solarea/explorer/react/tokens.jsx
@@ -1,10 +1,50 @@
 const BulmaCard = render('dev', 'bulma-card');
+const Link = render('dev', 'link');
 
+const { tokenRegExp } = await require('solarea://explorer/utils');
+console.log(tokenRegExp);
 add(() => {
+  const { data: tokenData, isLoading } = solarea.useQuery('tokendata_query', () =>
+    globalThis.fetch('https://evmexplorer.velas.com/tokens?type=JSON').then((res) => res.json()),
+  );
+
+  if (isLoading)
+    return (
+      <div className="bu-container bu-is-max-desktop">
+        <BulmaCard header="Loading token data . . ." />
+      </div>
+    );
+
   return (
     <Render id="explorer" name="layout">
       <div className="bu-container bu-is-max-desktop">
-        <BulmaCard header="Tokens"></BulmaCard>
+        <BulmaCard header="Tokens" />
+        <BulmaCard>
+          <div className="bu-columns bu-is-mobile">
+            <div className="bu-column bu-is-1">№</div>
+            <div className="bu-column bu-is-3">Token</div>
+            <div className="bu-column bu-is-5">Address</div>
+            <div className="bu-column bu-is-3" style={{ textAlign: 'end' }}>
+              Supply
+            </div>
+          </div>
+          {!isLoading &&
+            tokenData.items.map((tokenHtml, index) => {
+              const [, tokenName, tokenAddress, tokenSupply] = tokenRegExp.exec(tokenHtml);
+              return (
+                <div className="bu-columns bu-is-mobile">
+                  <div className="bu-column bu-is-1">{index + 1}</div>
+                  <div className="bu-column bu-is-3">{tokenName}</div>
+                  <div className="bu-column bu-is-5">
+                    <Link to={`/address/${tokenAddress}?chain=evm`}>{tokenAddress}</Link>
+                  </div>
+                  <div className="bu-column bu-is-3" style={{ textAlign: 'end' }}>
+                    {tokenSupply.slice(0, 15)}
+                  </div>
+                </div>
+              );
+            })}
+        </BulmaCard>
       </div>
     </Render>
   );
