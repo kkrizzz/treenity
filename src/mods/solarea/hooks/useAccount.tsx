0e3184ef2cb665b0bbc6f4cae5723abc0f5dafb1
Kurilov <kri.zka@gmail.com>
2021-08-16 13:33:30 +0500

    fix account changes

--- src/mods/solarea/hooks/useAccount.tsx
+++ src/mods/solarea/hooks/useAccount.tsx
@@ -1,10 +1,9 @@
 import * as React from 'react';
 import { useConnection } from './useConnection';
 import { useQuery } from 'react-query';
-import { AccountInfo, clusterApiUrl, Connection, PublicKey } from '@solana/web3.js';
-import { useMemo } from 'react';
+import { AccountInfo, PublicKey } from '@solana/web3.js';
 
-export function useAccount(address: strin): [AccountInfo<Buffer> | null, boolean] {
+export function useAccount(address: string): [AccountInfo<Buffer> | null, boolean] {
   const connection = useConnection();
   const { data: account, isLoading } = useQuery(`acc_${address}_${connection._rpcEndpoint}`, () =>
     connection.getAccountInfo(new PublicKey(address)),
