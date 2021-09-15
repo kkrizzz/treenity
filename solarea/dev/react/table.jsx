c1a30c6b6a3f2e028ef754379ba1a44e3f9d1c67
eugenebondarev <laser4622@gmail.com>
2021-09-15 14:45:04 +0300

    add theme support for Table component

--- solarea/dev/react/table.jsx
+++ solarea/dev/react/table.jsx
@@ -1,7 +1,31 @@
 const Table = ({ columns = [], data = [], stripped = false, bordered = false }) => {
+  useCSS(
+    'table',
+    css`
+      .bu-table.bu-is-striped tbody tr:not(.bu-is-selected):nth-child(even) {
+        background-color: rgba(178, 178, 178, 0.15) !important;
+      }
+
+      .bu-table td,
+      .bu-table th {
+        border-color: rgba(143, 143, 143, 0.3) !important;
+      }
+
+      .table {
+        color: var(--theme-main-color);
+        background-color: transparent !important;
+      }
+
+      .table th {
+        color: var(--theme-main-color) !important;
+      }
+    `,
+  );
   return (
     <table
-      className={`bu-table ${bordered ? 'bu-is-bordered' : ''} ${stripped ? 'bu-is-striped' : ''}`}
+      className={`bu-table table ${bordered ? 'bu-is-bordered' : ''} ${
+        stripped ? 'bu-is-striped' : ''
+      }`}
       style={{ width: '100%' }}
     >
       <thead>
@@ -16,11 +40,11 @@ const Table = ({ columns = [], data = [], stripped = false, bordered = false }) 
         {data.map((item) => (
           <tr>
             {columns.map((column) => (
-              <th style={{ textAlign: column.textAlign || 'left' }}>
+              <td style={{ textAlign: column.textAlign || 'left' }}>
                 {column.render
                   ? column.render(item[column.dataIndex], item)
                   : item[column.dataIndex]}
-              </th>
+              </td>
             ))}
           </tr>
         ))}
