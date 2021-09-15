3a9aeb715806ee94e64957b2ece05f735b949dcf
eugenebondarev <laser4622@gmail.com>
2021-09-15 09:27:19 +0300

    add Table component

---
+++ solarea/dev/react/table.jsx
@@ -0,0 +1,33 @@
+const Table = ({ columns = [], data = [], stripped = false, bordered = false }) => {
+  console.log(data[0]);
+  return (
+    <table
+      className={`bu-table ${bordered && 'bu-is-bordered'} ${stripped && 'bu-is-striped'}`}
+      style={{ width: '100%' }}
+    >
+      <thead>
+        <tr>
+          {columns.map((column) => (
+            <th style={{ textAlign: column.textAlign || 'left' }}>{column.title}</th>
+          ))}
+        </tr>
+      </thead>
+
+      <tbody>
+        {data.map((item) => (
+          <tr>
+            {columns.map((column) => (
+              <th style={{ textAlign: column.textAlign || 'left' }}>
+                {column.render
+                  ? column.render(item[column.dataIndex], item)
+                  : item[column.dataIndex]}
+              </th>
+            ))}
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+};
+
+add(Table);
