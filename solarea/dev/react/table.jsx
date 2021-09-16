d2a3fa83dceada04886c8442ae1fe621114d18a2
eugenebondarev <laser4622@gmail.com>
2021-09-16 10:49:52 +0300

    add new design for Table component

--- solarea/dev/react/table.jsx
+++ solarea/dev/react/table.jsx
@@ -3,30 +3,35 @@ const Table = ({ columns = [], data = [], stripped = false, bordered = false }) 
     'table',
     css`
       .bu-table.bu-is-striped tbody tr:not(.bu-is-selected):nth-child(even) {
-        background-color: rgba(178, 178, 178, 0.15) !important;
+        background-color: transparent !important;
+      }
+      .bu-table.bu-is-striped tbody tr:not(.bu-is-selected):nth-child(odd) {
+        background-color: var(--theme-subcard-bg-color) !important;
       }
 
       .bu-table td,
       .bu-table th {
-        border-color: rgba(143, 143, 143, 0.3) !important;
-      }
-
-      .table {
-        color: var(--theme-main-color);
-        background-color: transparent !important;
-      }
-
-      .table th {
+        border-color: transparent !important;
         color: var(--theme-main-color) !important;
+
+        font-weight: 600 !important;
+        font-size: 14px !important;
+        padding: 20px 0.75em;
+      }
+
+      .bu-table {
+        color: var(--theme-main-color);
+        font-weight: bold;
+        font-size: 16px;
+        line-height: 20px;
+        background-color: transparent !important;
+        width: 100%;
       }
     `,
   );
   return (
     <table
-      className={`bu-table table ${bordered ? 'bu-is-bordered' : ''} ${
-        stripped ? 'bu-is-striped' : ''
-      }`}
-      style={{ width: '100%' }}
+      className={`bu-table ${bordered ? 'bu-is-bordered' : ''} ${stripped ? 'bu-is-striped' : ''}`}
     >
       <thead>
         <tr>
