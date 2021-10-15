89cd17f33a8b1bc903dc0ea3f9a91f37e82c82c1
feddot2517 <8d2G1101>
2021-10-15 19:17:29 +0600

    make nft type resolver & add arveawe

--- solarea/near/react/new-account-nfts.jsx
+++ solarea/near/react/new-account-nfts.jsx
@@ -6,6 +6,51 @@ const {
   useNearTokens,
 } = await require('solarea://near/utils');
 
+const useResolveNftMedia = (token, collection) => {
+  const [url, setUrl] = React.useState(undefined);
+  const [isLoading, setIsLoading] = React.useState(true);
+
+  React.useEffect(() => {
+    (async function () {
+      const tokenMetadataMedia = token.metadata.media;
+      const baseUri = collection.metadata.base_uri;
+
+      if (!tokenMetadataMedia && !baseUri) {
+        setUrl('');
+        return setIsLoading(false);
+      }
+
+      if (tokenMetadataMedia && tokenMetadataMedia.startsWith('bafy')) {
+        setUrl(`https://ipfs.fleek.co/ipfs/${tokenMetadataMedia}`);
+      } else if (tokenMetadataMedia && tokenMetadataMedia.startsWith('http')) {
+        setUrl(tokenMetadataMedia);
+      } else if (baseUri && baseUri.includes('arweave')) {
+        const tokenMetadataFromArweave = await fetch(`${baseUri}/${token.metadata.reference}`)
+          .then((res) => res.json())
+          .catch((e) => console.error(e));
+        setUrl(tokenMetadataFromArweave.media);
+      }
+      setIsLoading(false);
+    })();
+  }, []);
+
+  return [url, isLoading];
+};
+
+const NftResolver = ({ token, collection }) => {
+  const [nftMediaUrl, isLoading] = useResolveNftMedia(token, collection);
+
+  return (
+    <div className="bu-column bu-is-4 bu-is-half-tablet">
+      {isLoading ? (
+        <span className="spinner-grow spinner-grow-sm m-r-4" />
+      ) : (
+        <img src={nftMediaUrl} />
+      )}
+    </div>
+  );
+};
+
 const NftWrapper = styled.div`
   background: white;
   font-size: 14px;
@@ -40,24 +85,15 @@ add(({ entityId }) => {
                 alignItems: 'center',
                 display: 'flex',
                 marginBottom: '1.5rem',
+                marginTop: '1.5rem',
               }}
             >
               <img width={32} src={nftCollection.metadata.icon} style={{ marginRight: '1rem' }} />
               <strong>{nftCollection.metadata.name}</strong>
             </div>
-            <div class="bu-columns">
+            <div class="bu-columns" style={{ flexFlow: 'wrap' }}>
               {nftCollection.tokens.map((i) => {
-                return (
-                  <div class="bu-column bu-is-4 bu-is-half-tablet">
-                    <img
-                      src={
-                        i.metadata.media.startsWith('bafy')
-                          ? `https://ipfs.fleek.co/ipfs/${i.metadata.media}`
-                          : i.metadata.media
-                      }
-                    />
-                  </div>
-                );
+                return <NftResolver collection={nftCollection} token={i} />;
               })}
             </div>
           </div>
